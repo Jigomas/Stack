aa22fc2400226c20932b9aa537e9c923d668396f
Jigomas <104555528+Jigomas@users.noreply.github.com>
2024-09-27 14:26:44 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -31,9 +31,11 @@ int Stk_Verifier(struct Stk_t *Stk);
 
 int Stk_Push(struct Stk_t *Stk, Stk_Elem_t New_Element);
 
-void Stk_Add_Mem(struct Stk_t **Stk);
+int Stk_Pop(struct Stk_t *Stk);
 
-int Stk_Pop();
+void Stk_Free_Mem(struct Stk_t **Stk);
+
+void Stk_Add_Mem(struct Stk_t **Stk);
 
 Stk_Elem_t Reader();
 
@@ -49,19 +51,42 @@ int main() {
 
         unsigned int Capacity = 5;    // this is gonna be starting Capacity
 
+
+
         Exit_Code *= Stk_Ctor(&Stk, Capacity);
         Stk_Assertion_Func(&Stk, Exit_Code);
 
-        Stk_Elem_t New_Element = Reader();
 
-        Exit_Code *= Stk_Push(&Stk,  New_Element);
-        Stk_Assertion_Func(&Stk, Exit_Code);
+        for (int i = 0; i < 10/*How many do u want to pop*/; i++) {
+            Stk_Elem_t New_Element = Reader();
+            Exit_Code *= Stk_Push(&Stk,  New_Element);
+            Stk_Assertion_Func(&Stk, Exit_Code);
+        }
 
-        for (int i = 0; i < Capacity; i++)
-            printf("%ld ", Stk.Data[i]);
 
+
+
+
+        for (int i = 0; i < Stk.Capacity; i++)
+            printf("%lf ", Stk.Data[i]);
+        printf("\n");
+
+
+        // Using pop
+        for (int i = 0; i < 10/*How many do u want to pop*/; i++) {
+            Exit_Code *= Stk_Pop(&Stk);
+            Stk_Assertion_Func(&Stk, Exit_Code);
+
+
+            for (int i = 0; i < Stk.Capacity; i++)
+                printf("%lf ", Stk.Data[i]);
+
+            printf("\n");
+        }
+        Exit_Code *= Stk_Dtor(&Stk);
 
         Exit_Code = SUCCESS;
+        printf("%d", Exit_Code);
     }
 }
 
@@ -82,16 +107,16 @@ int Stk_Push(struct Stk_t *Stk, Stk_Elem_t New_Element) {
 
     if (!Exit_Code) return Exit_Code;
 
-    //Stk_Add_Mem(&Stk);
+    Stk_Add_Mem(&Stk);
     printf("New_Element %lf\n", New_Element);
-    printf("Size b4 %d\n", Stk->Size);
+    //printf("Size b4 %d\n", Stk->Size);
+    //printf("%d, %d\n", Stk->Size, Stk->Capacity);
 
-
-    Stk->Data [0] = New_Element;
+    Stk->Data [Stk->Size] = New_Element;
     Stk->Size ++;
 
-    printf("Size after %d\n", Stk->Size);
-    
+    //printf("Size after %d\n", Stk->Size);
+
     return Exit_Code;
 }
 
@@ -100,6 +125,43 @@ int Stk_Push(struct Stk_t *Stk, Stk_Elem_t New_Element) {
 void Stk_Add_Mem(struct Stk_t **Stk) {
     if ((*Stk)->Size == (*Stk)->Capacity) {
         size_t New_Capacity = (*Stk)->Capacity ? (*Stk)->Capacity * 2 : 1; // Start with 1 if capacity is 0
+        Stk_Elem_t *New_Data = (Stk_Elem_t *)realloc((*Stk)->Data, New_Capacity * sizeof(Stk_Elem_t));
+
+        for (int i = (*Stk)->Size; i < New_Capacity; i++)
+              (*Stk)->Data [i] = 0;
+
+        if (New_Data == NULL) {
+            printf("Memory allocation failed\n");
+            return;
+        }
+
+        (*Stk)->Data = New_Data;
+        (*Stk)->Capacity = New_Capacity; // Update capacity
+    }
+}
+
+
+
+int Stk_Pop(struct Stk_t *Stk) {
+    int Exit_Code = Stk_Verifier(Stk);
+
+    if (!Exit_Code || Stk->Size <= 0) return Exit_Code;
+
+    Stk->Size --;
+    Stk_Free_Mem(&Stk);
+
+    printf("POP %lf\n", Stk->Data [Stk->Size]);
+    Stk->Data [Stk->Size] = 0;
+
+    return Exit_Code;
+}
+
+
+
+void Stk_Free_Mem(struct Stk_t **Stk) {
+    if ((*Stk)->Size <= 0.25 * (*Stk)->Capacity) {
+        size_t New_Capacity = (*Stk)->Capacity / 2;
+
         Stk_Elem_t *New_Data = (Stk_Elem_t *)realloc((*Stk)->Data, New_Capacity * sizeof(Stk_Elem_t));
 
         if (New_Data == NULL) {
@@ -112,8 +174,10 @@ void Stk_Add_Mem(struct Stk_t **Stk) {
     }
 }
 
+
+
 int Stk_Dtor(struct Stk_t *Stk) {
-    for (int i = 0; i < (Stk->Size) - 1; i++)
+    for (int i = 0; i < (Stk->Size); i++)
         Stk->Data [i] = 0;
 
     Stk->Capacity = 0;
